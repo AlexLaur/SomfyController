c247662f809f8643789215af6ebbe3ac7d8c9f57
Alexandre Laurette <laurette.alexandre@gmail.com>
2022-10-23 16:09:49 +0200

    update config

--- default_config.h
+++ default_config.h
@@ -5,27 +5,23 @@
  * @version 0.1
  * @date 2022-10-20
  *
+ * This file should be rename in "config.h".
+ *
  * @copyright Copyright (c) 2022
  *
  */
 #pragma once
-
-#include "remotes.h"
 
 const char SSID[] = "";
 const char PASSWORD[] = "";
 
 const int SERVER_PORT = 80;
 
-Remote DEFAULT_REMOTES [] = {
-    {0x100000, 0, "remote00", false},
-    {0x100001, 0, "remote01", false},
-    {0x100002, 0, "remote02", false},
-    {0x100003, 0, "remote03", false},
-    {0x100004, 0, "remote04", false},
-    {0x100005, 0, "remote05", false},
-    {0x100006, 0, "remote06", false},
-    {0x100007, 0, "remote07", false},
-    {0x100008, 0, "remote08", false},
-    {0x100009, 0, "remote09", false},
+// Remotes (max 32 remotes can be registered)
+const char* SOMFY_CONFIG_REMOTES[] = {
+    "Remote 00",
+    "Remote 01",
+    "Remote 02",
+    "Remote 03",
+    "Remote 04"
 };
