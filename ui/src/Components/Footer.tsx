757fccf3063840d2d570962759ba8dec01b8eab0
Alexandre Laurette <laurette.alexandre@gmail.com>
2022-10-27 14:09:51 +0200

    add documentation for API

--- ui/src/Components/Footer.tsx
+++ ui/src/Components/Footer.tsx
@@ -8,6 +8,7 @@ import {
   UnstyledButton,
   Anchor,
   Code,
+  Badge
 } from "@mantine/core";
 
 const Footer = () => {
@@ -19,7 +20,13 @@ const Footer = () => {
         <Grid justify="space-around">
           <Grid.Col xs={12} sm={8} md={8} lg={8}>
             <Text size="xl" weight={700} color="white" mb="10px">
-              Simple Mantine template
+              API
+            </Text>
+            <Text>
+              <Badge color="green">GET</Badge> /blind?action=[up, down, stop, prog, reset, enable, disable]&remote_id=[id]
+            </Text>
+            <Text>
+              <Badge color="green">GET</Badge> /remotes
             </Text>
           </Grid.Col>
 
